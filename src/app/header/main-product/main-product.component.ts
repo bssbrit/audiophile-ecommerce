0bcd18e9e000e386cae0a8e8c77ee11931a8d5ce
bssbrit <brunosabrito2@gmail.com>
2025-05-29 23:06:08 -0300

    create header and 2 components

---
+++ src/app/header/main-product/main-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-product',
+  imports: [],
+  templateUrl: './main-product.component.html',
+  styleUrl: './main-product.component.css'
+})
+export class MainProductComponent {
+
+}
